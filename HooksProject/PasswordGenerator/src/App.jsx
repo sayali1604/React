519ff4efc450ca3b22e8e7688a239449d0ee60dd
sayali1604meshram <sayali1604meshram@gmail.com>
2025-08-06 22:01:32 +0530

    Added logic for password generator

--- HooksProject/PasswordGenerator/src/App.jsx
+++ HooksProject/PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
@@ -10,7 +10,7 @@ function App() {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "@#$*"
+    if (charAllowed) str += "@#$*=-()+%!~"
     for (let i=1; i<= length; i++) {
         let char = Math.floor(Math.random() * str.length + 1)
         pass += str.charAt(char)
@@ -23,13 +23,22 @@ function App() {
     setPassword
   ])
 
+  const passwordRef = useRef()
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+  useEffect(()=>{
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+
   return (
     <>
       <h1 className='text-4xl text-center text-white'>Password Generator</h1>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-4 my-8 text-orange-500 bg-gray-700'>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-          <input type='text' value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly/>
-          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+          <input type='text' ref={passwordRef} value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly/>
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0' onClick={copyPasswordToClipboard}>Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
            <div className='flex items-center gap-x-1'>
